c27d772c481e81877f4b4f69fefde70a7b119425
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-10 19:46:01 +0000

    Employee model update to allow for authentication/registration

--- web/models/employee.ex
+++ web/models/employee.ex
@@ -1,19 +1,37 @@
 defmodule Appointments.Employee do
   use Appointments.Web, :model
 
+  # TODO(krummi): alias Openmaize.DB
+
   schema "employees" do
     field :name, :string
     field :phone, :string
-    field :email, :string
     field :bio, :string
     field :avatar_url, :string
+
+    # Openmaize stuff
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    # Email confirmation
+    field :confirmation_token, :string
+    field :confirmation_sent_at, Ecto.DateTime
+    field :confirmed_at, Ecto.DateTime
+
+    # Password reset fields
+    field :reset_token, :string
+    field :reset_sent_at, Ecto.DateTime
+
     belongs_to :company, Appointments.Company
 
     timestamps
   end
 
-  @required_fields ~w(name phone email bio avatar_url)
-  @optional_fields ~w()
+  @required_fields ~w(name email password_hash)
+  @optional_fields ~w(phone bio avatar_url
+                      confirmation_token confirmation_sent_at confirmed_at
+                      reset_token reset_sent_at)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -22,7 +40,9 @@ defmodule Appointments.Employee do
   with no validation performed.
   """
   def changeset(model, params \\ :empty) do
+    # TODO(krummi): validations
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:email)
   end
 end

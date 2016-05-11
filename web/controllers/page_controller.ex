c75d1c2057d90f7fb522de0cabaf5ee3c5878ff3
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-11 23:01:09 +0000

    Changes the contents of the JWT to meet our needs

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -8,12 +8,34 @@ defmodule Appointments.PageController do
     IO.puts("confirming: #{email}")
   end
 
+  # Taken from: https://github.com/riverrun/openmaizejwt/blob/master/lib/openmaize_jwt/plug.ex
+  import OpenmaizeJWT.Create
+  def add_token(conn, user, {storage, uniq}) do
+    company = Repo.get!(Appointments.Company, user.company_id)
+    user = Map.take(user, [:id, :name, :role, :company_id, uniq])
+    |> Map.put(:company_name, company.name)
+    # TODO(krummi): Make 120 a configuration option.
+    {:ok, token} = generate_token user, {0, 120}
+    put_token(conn, user, token, storage)
+  end
+  defp put_token(conn, user, token, :cookie) do
+    put_resp_cookie(conn, "access_token", token, [http_only: true])
+    |> put_private(:openmaize_user, user)
+  end
+  defp put_token(conn, user, token, nil) do
+    resp(conn, 200, ~s({"access_token": "#{token}"}))
+    |> put_private(:openmaize_user, user)
+  end
+
   plug Openmaize.ConfirmEmail, [key_expires_after: 30,
     mail_function: &Appointments.PageController.receipt_confirm/1] when action in [:confirm]
   plug Openmaize.ResetPassword, [key_expires_after: 30,
     mail_function: &Appointments.PageController.receipt_confirm/1] when action in [:reset_password]
 
-  plug Openmaize.Login, [unique_id: :email] when action in [:login_user]
+  plug Openmaize.Login, [
+    unique_id: :email,
+    add_jwt: &Appointments.PageController.add_token/3
+  ] when action in [:login_user]
   plug Openmaize.Logout when action in [:logout]
 
   def send_an_email(email, link) do

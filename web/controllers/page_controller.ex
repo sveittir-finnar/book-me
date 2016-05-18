c2509fe7c76835d7ecccaef5c3601529781c0d88
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-18 18:03:44 +0000

    POST /registration is now a JSON API endpoint

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -51,32 +51,45 @@ defmodule Appointments.PageController do
     render conn, "login.html"
   end
 
-  def registration(conn, params) do
+  def registration(conn, _params) do
     render conn, "registration.html"
   end
 
-  def registration_post(conn, %{"reg" => registration_params}) do
-      company_changeset = Company.changeset(%Company{}, %{
-        name: registration_params["company_name"]
-      })
-      employee_changeset = Employee.changeset(%Employee{}, %{
-        name: registration_params["first_name"] <> registration_params["last_name"],
-        email: registration_params["email"],
-        role: "full"
-      })
+  defp registration_err(conn, changesets) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(Appointments.ChangesetView, "error.json", errors: changesets)
+  end
 
-      Repo.transaction fn ->
-        company = Repo.insert!(company_changeset)
-        employee = Ecto.build_assoc(company, :employees, employee_changeset)
-        Repo.insert!(employee)
+  def registration_post(conn, %{"employee" => employee_params, "company" => company_params}) do
+    employee_params = Map.put(employee_params, "role", "full")
+    # HACK: Set this temporarily so the company_id validation will not trigger
+    employee_params = Map.put(employee_params, "company_id", -1)
+
+    company_changeset = Company.changeset(%Company{}, company_params)
+    employee_changeset = Employee.changeset(%Employee{}, employee_params)
+
+    if company_changeset.valid? && employee_changeset.valid? do
+      # TODO(krummi): Use Ecto.Multi when we start using Ecto 2.0
+      #               See: https://github.com/elixir-lang/ecto/issues/1114
+      case Repo.transaction fn ->
+        case Repo.insert(company_changeset) do
+          {:ok, company} ->
+            employee_changeset = Ecto.Changeset.put_change(employee_changeset, :company_id, company.id)
+            case Repo.insert(employee_changeset) do
+              {:ok, employee} -> employee
+              {:error, changeset} -> Repo.rollback(%{employee: changeset, company: company_changeset})
+            end
+          {:error, changeset} ->
+            Repo.rollback(%{employee: employee_changeset, company: changeset})
+        end
+      end do
+        {:ok, _} ->
+          conn |> put_status(:created) |> render(Appointments.ShowView, "show.json", res: "good")
+        {:error, changesets} -> registration_err(conn, changesets)
       end
-      conn
-      |> redirect(to: page_path(conn, :index))
     else
-      IO.inspect(changeset)
-      conn
-      |> put_flash(:info, "Please correct the following errors in your form.")
-      render(conn, "registration.html", changeset: changeset)
+      registration_err(conn, %{employee: employee_changeset, company: company_changeset})
     end
   end
 

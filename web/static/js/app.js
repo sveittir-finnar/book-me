463fc4a6efefb153f60a08ab2543b58e4fa303a0
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-19 23:47:49 +0000

    Add lodash and jquery dependencies

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,8 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import _ from "lodash"
+import $ from "jquery"
+
+import registration from "./registration"

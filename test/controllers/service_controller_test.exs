d9cf7e3b9f60477d25f3a9e6bd9f98ae50cb53c8
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-21 13:58:03 +0000

    Fix tests for the service controller

--- test/controllers/service_controller_test.exs
+++ test/controllers/service_controller_test.exs
@@ -2,14 +2,15 @@ defmodule Appointments.ServiceControllerTest do
   use Appointments.ConnCase
 
   alias Appointments.Service
-  @valid_service %{
-    id: "7488a646-e31f-11e4-aace-600308960662",
-    name: "Bike bag rental",
-    description: "EVOC bag",
-    duration: 5,
-    pricing: "5000 kr/klst"
-  }
-  @invalid_service %{}
+
+  @invalid_attrs %{name: "testing"}
+
+  setup do
+    company = insert(:company)
+    valid_attrs = %{name: "Haircut", duration: 45, company_id: company.id}
+
+    {:ok, conn: conn, company: company, valid_attrs: valid_attrs}
+  end
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, service_path(conn, :index)
@@ -21,50 +22,58 @@ defmodule Appointments.ServiceControllerTest do
     assert html_response(conn, 200) =~ "New service"
   end
 
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, service_path(conn, :create), service: @valid_service
+  test "creates resource and redirects when data is valid",
+    %{conn: conn, valid_attrs: valid_attrs} do
+    conn = post conn, service_path(conn, :create), service: valid_attrs
     assert redirected_to(conn) == service_path(conn, :index)
-    assert Repo.get_by(Service, @valid_service)
+    assert Repo.get_by(Service, valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, service_path(conn, :create), service: @invalid_service
+  test "does not create resource and renders errors when data is invalid",
+    %{conn: conn} do
+    conn = post conn, service_path(conn, :create), service: @invalid_attrs
     assert html_response(conn, 200) =~ "New service"
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    service = Repo.insert! %Service{}
+  test "shows chosen resource", %{conn: conn, valid_attrs: valid_attrs} do
+    service = Repo.insert! Service.changeset(%Service{}, valid_attrs)
     conn = get conn, service_path(conn, :show, service)
     assert html_response(conn, 200) =~ "Show service"
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
-      get conn, service_path(conn, :show, -1)
+      # This should be unlikely enough:
+      get conn, service_path(conn, :show, Ecto.UUID.generate)
     end
   end
 
-  test "renders form for editing chosen resource", %{conn: conn} do
-    service = Repo.insert! %Service{}
+  test "renders form for editing chosen resource",
+    %{conn: conn, valid_attrs: valid_attrs} do
+    service = Repo.insert! Service.changeset(%Service{}, valid_attrs)
     conn = get conn, service_path(conn, :edit, service)
     assert html_response(conn, 200) =~ "Edit service"
   end
 
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    service = Repo.insert! %Service{}
-    conn = put conn, service_path(conn, :update, service), service: @valid_service
+  test "updates chosen resource and redirects when data is valid",
+    %{conn: conn, valid_attrs: valid_attrs} do
+    service = Repo.insert! Service.changeset(%Service{}, valid_attrs)
+    update_attrs = Map.put(valid_attrs, :cleanup_duration, 15)
+    conn = put conn, service_path(conn, :update, service), service: update_attrs
     assert redirected_to(conn) == service_path(conn, :show, service)
-    assert Repo.get_by(Service, @valid_service)
+    assert Repo.get_by(Service, update_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    service = Repo.insert! %Service{}
-    conn = put conn, service_path(conn, :update, service), service: @invalid_service
+  test "does not update chosen resource and renders errors when data is invalid",
+    %{conn: conn, valid_attrs: valid_attrs} do
+    service = Repo.insert! Service.changeset(%Service{}, valid_attrs)
+    update_attrs = Map.put(valid_attrs, :cleanup_duration, -1)
+    conn = put conn, service_path(conn, :update, service), service: update_attrs
     assert html_response(conn, 200) =~ "Edit service"
   end
 
-  test "deletes chosen resource", %{conn: conn} do
-    service = Repo.insert! %Service{}
+  test "deletes chosen resource", %{conn: conn, valid_attrs: valid_attrs} do
+    service = Repo.insert! Service.changeset(%Service{}, valid_attrs)
     conn = delete conn, service_path(conn, :delete, service)
     assert redirected_to(conn) == service_path(conn, :index)
     refute Repo.get(Service, service.id)

9228d2eb22a4ad829da415fd80da44782f3fe344
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-21 22:08:45 +0000

    Update the company controller and its tests

--- test/controllers/company_controller_test.exs
+++ test/controllers/company_controller_test.exs
@@ -2,36 +2,30 @@ defmodule Appointments.CompanyControllerTest do
   use Appointments.ConnCase
 
   alias Appointments.Company
-  @valid_attrs %{description: "some content",
-                 email: "some content",
-                 name: "some content",
-                 phone: "some content"}
-  @invalid_attrs %{}
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, company_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing companies"
+  @valid_attrs %{name: "Hudsucker Industries"}
+  @invalid_attrs %{name: ""}
+
+  setup do
+    company = insert(:company)
+    employee = insert(:employee, company_id: company.id)
+
+    {:ok, token} = create_token(company, employee)
+    conn = conn() |> put_req_cookie("access_token", token)
+
+    {:ok, conn: conn, company: company}
   end
 
-  test "renders form for new resources", %{conn: conn} do
-    conn = get conn, company_path(conn, :new)
-    assert html_response(conn, 200) =~ "New company"
+  # Company public page
+
+  test "shows a company public page to an anonymous user", %{company: company} do
+    conn = get conn(), company_path(conn, :show, company.id)
+    assert html_response(conn, 200) =~ "Show company"
   end
 
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, company_path(conn, :create), company: @valid_attrs
-    assert redirected_to(conn) == company_path(conn, :index)
-    assert Repo.get_by(Company, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, company_path(conn, :create), company: @invalid_attrs
-    assert html_response(conn, 200) =~ "New company"
-  end
-
-  test "shows chosen resource", %{conn: conn} do
-    company = Repo.insert! %Company{}
-    conn = get conn, company_path(conn, :show, company)
+  test "shows a company public page to an logged in user",
+    %{conn: conn, company: company} do
+    conn = get conn, company_path(conn, :show, company.id)
     assert html_response(conn, 200) =~ "Show company"
   end
 
@@ -42,28 +36,20 @@ defmodule Appointments.CompanyControllerTest do
   end
 
   test "renders form for editing chosen resource", %{conn: conn} do
-    company = Repo.insert! %Company{}
-    conn = get conn, company_path(conn, :edit, company)
+    conn = get conn, company_path(conn, :edit)
     assert html_response(conn, 200) =~ "Edit company"
   end
 
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    company = Repo.insert! %Company{}
-    conn = put conn, company_path(conn, :update, company), company: @valid_attrs
+  test "updates chosen resource and redirects when data is valid",
+    %{conn: conn, company: company} do
+    conn = patch conn, company_path(conn, :update), company: @valid_attrs
     assert redirected_to(conn) == company_path(conn, :show, company)
     assert Repo.get_by(Company, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    company = Repo.insert! %Company{}
-    conn = put conn, company_path(conn, :update, company), company: @invalid_attrs
+  test "does not update chosen resource and renders errors when data is invalid",
+    %{conn: conn} do
+    conn = patch conn, company_path(conn, :update), company: @invalid_attrs
     assert html_response(conn, 200) =~ "Edit company"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    company = Repo.insert! %Company{}
-    conn = delete conn, company_path(conn, :delete, company)
-    assert redirected_to(conn) == company_path(conn, :index)
-    refute Repo.get(Company, company.id)
   end
 end

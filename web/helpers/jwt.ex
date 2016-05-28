9b7c0fb78fafbdf1bf85f16eb5245ff47722ecab
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-05-28 11:31:10 +0000

    Get rid of unused variable warning

--- web/helpers/jwt.ex
+++ web/helpers/jwt.ex
@@ -5,16 +5,16 @@ defmodule Appointments.JWT do
   alias Appointments.{Company, Repo}
 
   # Based on: https://github.com/riverrun/openmaizejwt/blob/master/lib/openmaize_jwt/plug.ex
-  def create_token(conn, user, storage, uniq) do
+  def create_token(user, uniq) do
     company = Repo.get!(Company, user.company_id)
     user = Map.take(user, [:id, :first_name, :role, :company_id, uniq])
     |> Map.put(:company_name, company.name)
     # TODO(krummi): Make 120 a configuration option.
-    {:ok, token} = generate_token user, {0, 120}
+    generate_token(user, {0, 120})
   end
 
   def add_token(conn, user, {storage, uniq}) do
-    {:ok, token} = create_token(conn, user, storage, uniq)
+    {:ok, token} = create_token(user, uniq)
     put_token(conn, user, token, storage)
   end
 

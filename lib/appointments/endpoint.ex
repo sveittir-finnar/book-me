827631a90aa99a5d4b77ac49a8d937c8896d88f7
Kári Tristan Helgason <kthelgason@gmail.com>
2016-06-08 09:26:08 +0000

    Serve uploaded images from fs

--- lib/appointments/endpoint.ex
+++ lib/appointments/endpoint.ex
@@ -11,6 +11,8 @@ defmodule Appointments.Endpoint do
     at: "/", from: :appointments, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+  plug Plug.Static, at: "/uploads", from: "uploads", gzip: true
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do

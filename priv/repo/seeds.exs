35cee17d4c520d701e404eca961605548240284e
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-06-08 22:45:03 +0000

    Update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,11 +10,13 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Appointments.{Repo, Company, Employee, Service, Client}
+alias Appointments.{Repo, Company, Employee, Service, Client, Reservation}
 import Openmaize.DB
+use Timex
 
-# Clean everything up
+# Clean everything up (in the correct order)
 
+Repo.delete_all(Reservation)
 Repo.delete_all(Client)
 Repo.delete_all(Service)
 Repo.delete_all(Employee)
@@ -22,7 +24,7 @@ Repo.delete_all(Company)
 
 # Create companies
 
-company1 = Repo.insert! %Company{
+company1 = Repo.insert!(%Company{
   name: "Hársnyrtifélagið Gæðahár",
   phone: "+354 823 9745",
   email: "harsnyrtifelagid@gmail.com",
@@ -43,13 +45,13 @@ company1 = Repo.insert! %Company{
     fri: [[9, 12], [13, 17]],
     sat: [[13, 16]]
   }
-}
+})
 
-company2 = Repo.insert! %Company{
+company2 = Repo.insert!(%Company{
   name: "Tannlæknastofa Héðins",
   phone: "+354 478 1345",
   email: "tannlaeknastofan@gmail.com"
-}
+})
 
 IO.inspect(company1)
 
@@ -57,8 +59,7 @@ IO.inspect(company1)
 
 key = "a"
 
-%Employee{}
-|> Employee.auth_changeset(%{
+employee1 = Employee.auth_changeset(%Employee{}, %{
   email: "test@test.com",
   first_name: "Ari Emil",
   last_name: "Atlason",
@@ -68,12 +69,9 @@ key = "a"
   phone: "+354 823 9745",
   avatar_url: "https://avatars2.githubusercontent.com/u/331083?v=3&s=460",
   company_id: company1.id
-}, key)
-|> Repo.insert!
-|> user_confirmed
+}, key) |> Repo.insert!
 
-%Employee{}
-|> Employee.auth_changeset(%{
+employee2 = Employee.auth_changeset(%Employee{}, %{
   email: "siggi@test.com",
   first_name: "Sigurður",
   last_name: "Sigurðsson",
@@ -83,12 +81,12 @@ key = "a"
   phone: "+354 848 4293",
   avatar_url: "https://avatars3.githubusercontent.com/u/2563784?v=3&s=400",
   company_id: company1.id
-}, key)
-|> Repo.insert!
-|> user_confirmed
+}, key) |> Repo.insert!
 
-%Employee{}
-|> Employee.auth_changeset(%{
+user_confirmed(employee1)
+user_confirmed(employee2)
+
+Employee.auth_changeset(%Employee{}, %{
   email: "gisli@test.com",
   first_name: "Gísli Marteinn",
   last_name: "Baldursson",
@@ -104,39 +102,34 @@ key = "a"
 
 # Create services
 
-%Service{}
-|> Service.changeset(%{
+service1 = Service.changeset(%Service{}, %{
   name: "Haircut: Crew cut",
   description: "Look ridiculously cool; get a crew cut!",
   duration: 30,
   cleanup_duration: 0,
   pricing: "4500 kr.",
   company_id: company1.id
-})
-|> Repo.insert!
+}) |> Repo.insert!
 
-%Service{}
-|> Service.changeset(%{
+Service.changeset(%Service{}, %{
   name: "Haircut: Mohawk cut",
   description: "Mohawk haircuts are the shit this summer!",
   duration: 45,
   cleanup_duration: 15,
   company_id: company1.id
-})
-|> Repo.insert!
+}) |> Repo.insert!
 
-%Service{}
-|> Service.changeset(%{
+
+Service.changeset(%Service{}, %{
   name: "Dentist appointment",
   description: "A classic mouth wash!",
   duration: 60,
   company_id: company2.id
-})
-|> Repo.insert!
+}) |> Repo.insert!
 
 # Create clients
 
-Client.changeset(%Client{}, %{
+client1 = Client.changeset(%Client{}, %{
   first_name: "Gisli Marteinn",
   last_name: "Baldursson",
   email: "gmb@gmb.is",
@@ -156,4 +149,23 @@ Client.changeset(%Client{}, %{
   first_name: "Kjartan",
   last_name: "Marteinsson",
   company_id: company2.id
+}) |> Repo.insert!
+
+# Create personal reservations
+
+Reservation.personal_changeset(%Reservation{}, %{
+  company_id: company1.id,
+  type: "personal",
+  start_time: DateTime.now(),
+  title: "Hvítasunnudagurinn",
+  employee_id: employee1.id
+}) |> Repo.insert!
+
+Reservation.client_changeset(%Reservation{}, %{
+  company_id: company1.id,
+  type: "client",
+  start_time: DateTime.now(),
+  duration: 45,
+  client_id: client1.id,
+  service_id: service1.id
 }) |> Repo.insert!

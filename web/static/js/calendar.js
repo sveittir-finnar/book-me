c75644a1c1bb9a020d2f0d57338b7a01f74143a9
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-06-10 22:34:29 +0000

    Internal reservation form + JavaScript for it

--- web/static/js/calendar.js
+++ web/static/js/calendar.js
@@ -44,10 +44,43 @@ function populate (calendar) {
 
 $(() => {
   let calendar = $('#calendar');
-  if (calendar.length > 0) {
-    populate(calendar);
-
-    // Populate clients
-    $("#client-id").select2();
+  if (calendar.length === 0) {
+    return;
   }
+
+  populate(calendar);
+
+  $('#reservation-form').submit(event => {
+    console.log($('#reservation-form').serializeArray());
+    event.preventDefault();
+  });
+
+  $('#reservation-client-id').select2();
+  $('#reservation-employee-id').select2();
+
+  // If all_day is checked, the start time should be disabled.
+  $('#reservation-all-day').change(() => {
+    let isChecked = $('#reservation-all-day').is(':checked');
+    if (isChecked) {
+      $('#reservation-start-time').attr('disabled', 'true');
+    } else {
+      $('#reservation-start-time').removeAttr('disabled');
+    }
+  });
+
+  // Default the duration of the appointment to that of the service's
+  $('#reservation-service-id').change(() => {
+    let serviceId = $('#reservation-service-id').val();
+    if (serviceId !== '') {
+      let option = $(`#reservation-service-id > option[value="${serviceId}"]`);
+      let duration = option.data('duration');
+      let hours = Math.floor(duration / 60);
+      let minutes = duration % 60;
+      $('#reservation-duration-hours').val(hours);
+      $('#reservation-duration-minutes').val(minutes);
+      $('#service-details').show();
+    } else {
+      $('#service-details').hide();
+    }
+  });
 });

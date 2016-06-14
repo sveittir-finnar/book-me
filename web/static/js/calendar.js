f2f7c77e121e26e199b135369f771ddaa67b1e3c
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-06-14 17:01:00 +0000

    Validation for reservations

--- web/static/js/calendar.js
+++ web/static/js/calendar.js
@@ -41,6 +41,69 @@ function populate (calendar) {
   });
 }
 
+function isUuid(value) {
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
+}
+
+function isDate(value) {
+  return /^\d{4}-\d{2}-\d{2}$/i.test(value);
+}
+
+function isTime(value) {
+  return /^\d{2}:\d{2}$/i.test(value);
+}
+
+function clearErrors() {
+  $('.errors').hide();
+}
+
+function showError(elem, message) {
+  elem.html(`<span style="color: red">${message}</span>`);
+  elem.show();
+}
+
+function validate(form) {
+  // Date & time
+  let startDate = $('#reservation-start-date');
+  if (!isDate(startDate.val())) {
+    showError($('#reservation-start-date-err'), 'You must pick a date!');
+  }
+  let allDay = $('#reservation-all-day');
+  let startTime = $('#reservation-start-time');
+  if (!allDay.is(':checked') && !isTime(startTime.val())) {
+    showError($('#reservation-start-time-err'),
+      'You must either pick a time OR an all day event!');
+  }
+
+  let type = $('#reservation-type').val();
+  if (type === 'client') {
+    // Validations specific to client reservations
+    // Client ID
+    let clientId = $('#reservation-client-id');
+    if (!isUuid(clientId.val())) {
+      showError($('#reservation-client-id-err'), 'You must choose a client!');
+    }
+
+    // Service ID
+    let serviceId = $('#reservation-service-id');
+    if (!isUuid(serviceId) && serviceId !== 'other') {
+      showError($('#reservation-service-id-err'),
+        'You must either choose a service or "other" and fill in the details.');
+    }
+  } else if (type === 'personal') {
+    // Validations specific to personal reservations
+    let employeeId = $('#reservation-employee-id').val();
+    if (!isUuid(employeeId)) {
+      showError($('#reservation-employee-id-err'),
+        'You must select the employee.');
+    }
+  }
+}
+
+function formToReservation(form) {
+
+}
+
 
 $(() => {
   let calendar = $('#calendar');
@@ -51,12 +114,45 @@ $(() => {
   populate(calendar);
 
   $('#reservation-form').submit(event => {
-    console.log($('#reservation-form').serializeArray());
+    // Start by validating the form
+    clearErrors();
+    validate();
+
+    // Serialize it to a "reservation" object
+    let arr = $('#reservation-form').serializeArray();
+    // [{ name: 'a', value: 1 }, { name: 'b', value: 3 }] => {a: 1, b: 3}
+    let form = _.zipObject(_.map(arr, 'name'), _.map(arr, 'value'));
+    console.log(form);
+
     event.preventDefault();
   });
 
   $('#reservation-client-id').select2();
   $('#reservation-employee-id').select2();
+
+  $('#personal-btn').click(() => {
+    $('#reservation-type').val('personal');
+
+    $('#client').hide();
+    $('#service').hide();
+    $('#title').show();
+
+    clearErrors();
+
+    $('#reservation-employee-id > option[value=""]').text('Select an employee');
+  });
+
+  $('#client-btn').click(() => {
+    $('#reservation-type').val('client');
+
+    $('#client').show();
+    $('#service').show();
+    $('#title').hide();
+
+    clearErrors();
+
+    $('#reservation-employee-id > option[value=""]').text('Any employee');
+  });
 
   // If all_day is checked, the start time should be disabled.
   $('#reservation-all-day').change(() => {
@@ -83,4 +179,6 @@ $(() => {
       $('#service-details').hide();
     }
   });
+
+  $('#client-btn').trigger('click');
 });
